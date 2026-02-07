e0e0b295af062b2282523dac706c0c18503e6649
Gabriel Github <Gabi04@live.dk>
2026-02-07 13:22:13 +0100

    Didnt want it anyways

--- frontend/src/components/selector/observerSelector/ObserverSelector.jsx
+++ frontend/src/components/selector/observerSelector/ObserverSelector.jsx
@@ -1,37 +1,8 @@
 import "../Selector.css";
 import PuzzlePiece from "../../puzzlePiece/PuzzlePiece";
 
-export default function ObserverSelector({ catalog, catalogLoading, catalogError }) {
+export default function ObserverSelector({ catalog }) {
     const observers = catalog?.observers ?? [];
-
-    if (catalogLoading) {
-        return (
-            <div className="selector-container">
-                <div className="selector-title">Select Observer</div>
-                <div className="option-container">
-                    <div className="option-list">
-                        <div>Loading...</div>
-                    </div>
-                    <div className="option-description"></div>
-                </div>
-            </div>
-        );
-    }
-
-    if (catalogError) {
-        return (
-            <div className="selector-container">
-                <div className="selector-title">Select Observer</div>
-                <div className="option-container">
-                    <div className="option-list">
-                        <div>Error loading catalog: {catalogError}</div>
-                    </div>
-                    <div className="option-description"></div>
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="selector-container">
             <div className="selector-title">Select Observer</div>
